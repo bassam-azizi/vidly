e8573c2813ca54c2918274c8d60071593d30e7ab
bassem <101010bassam@gmail.com>
2019-09-28 23:09:52 +0200

    Updates

---
+++ precache-manifest.0803c488f1d18fc631b12e8daa87f39a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73610efd3b0e811a606f7c534e3eff6c",
+    "url": "/vidly/index.html"
+  },
+  {
+    "revision": "d11f48579bf72ef6df05",
+    "url": "/vidly/static/css/2.e02f45ba.chunk.css"
+  },
+  {
+    "revision": "39ff37a3d68a6f06f004",
+    "url": "/vidly/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d11f48579bf72ef6df05",
+    "url": "/vidly/static/js/2.8767642f.chunk.js"
+  },
+  {
+    "revision": "39ff37a3d68a6f06f004",
+    "url": "/vidly/static/js/main.53ff8c38.chunk.js"
+  },
+  {
+    "revision": "2ba140b103883151b90b",
+    "url": "/vidly/static/js/runtime-main.617814cd.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/vidly/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/vidly/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/vidly/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/vidly/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/vidly/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
