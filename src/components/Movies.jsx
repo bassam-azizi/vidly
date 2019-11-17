09dfa658134841b76cd7e2a54d68de47ceeaf70d
bassem <101010bassam@gmail.com>
2019-11-17 20:56:18 +0100

    Movies component is updated

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,52 +1,96 @@
 import React, { Component } from "react";
+import MoviesTable from "./moviesTable";
+import Pagination from "./common/pagination";
+import ListGroup from "./common/listGroup";
 import { getMovies } from "../services/fakeMovieService";
+import { getGenres } from "../services/fakeGenreService";
+import { paginate } from "../utils/paginate";
 
 class Movies extends Component {
   state = {
-    movies: getMovies()
+    movies: [],
+    genres: [],
+    currentPage: 1,
+    pageSize: 4,
+    sortedColumn: {path: 'title', order: 'asc'}    
   };
+  componentDidMount() {
+    //  This is the best place to communicate and get your data from the server
+    // this is how to add a new element to a dataBase but just on the end part
+    const genres = [{ _id: "", name: "All Genres" }, ...getGenres()];
+
+    this.setState({ movies: getMovies(), genres });
+  }
+
+  render() {
+    const { length: count } = this.state.movies;
+
+    const {
+      selectedGenre,
+      currentPage,
+      pageSize,
+      movies: allMovies,
+      genres
+    } = this.state;
+
+    if (count === 0) return <h3>There are no movies showing on the list .</h3>;
+
+    const filtered =
+      selectedGenre && selectedGenre._id
+        ? allMovies.filter(m => m.genre._id === selectedGenre._id)
+        : allMovies;
+
+    const movies = paginate(filtered, currentPage, pageSize);
+
+    return (
+      <div className="row">
+        <div className="col-3 m-2">
+          <ListGroup
+            items={genres}
+            selectedItem={selectedGenre}
+            onGenreSelected={this.handelGenreSelected}
+          />
+        </div>
+        <div className="col">
+          <p>Showing {filtered.length} movies on the list .</p>
+          <MoviesTable
+            movies={movies}
+            onLike={this.handelLike}
+            onDelete={this.handelDelete}
+            onSort={this.handelSort}
+          />
+          <Pagination
+            itemCount={filtered.length}
+            pageSize={pageSize}
+            currentPage={currentPage}
+            pageSelect={this.handelPageSelected}
+          />
+        </div>
+      </div>
+    );
+  }
   handelDelete = movie => {
     const movies = this.state.movies.filter(m => m._id !== movie._id);
     this.setState({ movies });
   };
-  render() {
-    const { length: count } = this.state.movies;
-    if (count === 0) return <h3>There are no movies showing on the list .</h3>;
-    return (
-      <React.Fragment>
-        <h3>There are {count} movies showing on the list .</h3>
-        <table className="table table-striped">
-          <thead className="thead-dark">
-            <tr>
-              <th>Title</th>
-              <th>Genre</th>
-              <th>Stock</th>
-              <th>Rate</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.movies.map(movie => (
-              <tr key={movie._id}>
-                <td>{movie.title}</td>
-                <td>{movie.genre.name}</td>
-                <td>{movie.numberInStock}</td>
-                <td>{movie.dailyRentalRate}</td>
-                <td>
-                  <button
-                    onClick={() => this.handelDelete(movie)}
-                    className="btn btn-danger btn-sm"
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </React.Fragment>
-    );
-  }
+  handelLike = movie => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
+  };
+  handelPageSelected = page => {
+    this.setState({ currentPage: page });
+  };
+
+  handelGenreSelected = genre => {
+    this.setState({ selectedGenre: genre, currentPage: 1 });
+  };
+
+  handelSort = path => {
+    this.setState({ sortColumn: { path, order: "asc" } });
+  };
 }
 
 export default Movies;
